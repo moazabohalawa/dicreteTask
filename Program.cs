0fdb215e8bc77d651e1d0a5f617e548c6445504e
moazabohalawa <121347934+moazabohalawa@users.noreply.github.com>
2022-12-24 16:54:29 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number one");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number two");
+            int num2 = int.Parse(Console.ReadLine());
+
+            for (int primenum=num1; primenum<=num2; primenum++)
+            {
+                int ifvariable = 0;
+                for(int vloop =1;vloop <= primenum;vloop++)
+                {
+                    if(primenum % vloop == 0)
+                    {
+                        ifvariable++;
+                    }
+                }
+                if (ifvariable == 2)
+                {
+                    Console.WriteLine("prime number is:" + primenum);
+                }
+            }
+
+
+        }
+    }
+}
